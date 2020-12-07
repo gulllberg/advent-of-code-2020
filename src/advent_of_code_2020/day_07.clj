8912f5706cffe7fc20249d82bd7162228a05b5df
Daniel Gullberg <daniel@teamhex.se>
2020-12-07 16:46:12 +0100

    Refactored day 7 to use regexp

--- src/advent_of_code_2020/day_07.clj
+++ src/advent_of_code_2020/day_07.clj
@@ -4,32 +4,38 @@
 
 (def input (slurp "src/advent_of_code_2020/inputs/day07.txt"))
 
-; TODO: Do with regexp instead
 (defn parse-rule
   {:test (fn []
-           (is= (parse-rule "dotted maroon bags contain 4 plaid turquoise bags, 5 pale bronze bags.")
+           (is= (parse-rule "dotted maroon bags contain 4 plaid turquoise bags, 5 pale bronze bags."
+                            #"[a-z]+ [a-z]+")
                 ["dotted maroon" "plaid turquoise" "pale bronze"])
-           (is= (parse-rule "plaid turquoise bags contain no other bags.")
+           (is= (parse-rule "plaid turquoise bags contain no other bags."
+                            #"[a-z]+ [a-z]+")
+                ["plaid turquoise"])
+           (is= (parse-rule "dotted maroon bags contain 4 plaid turquoise bags, 5 pale bronze bags."
+                            #"\d*[ ]?[a-z]+ [a-z]+")
+                ["dotted maroon" "4 plaid turquoise" "5 pale bronze"])
+           (is= (parse-rule "plaid turquoise bags contain no other bags."
+                              #"\d*[ ]?[a-z]+ [a-z]+")
                 ["plaid turquoise"]))}
-  [rule]
-  (let [[parent rest] (clojure.string/split rule #" bags contain ")
-        children (reduce (fn [a c]
-                           (if (= c "no other bags.")
-                             a
-                             (conj a (clojure.string/join " " (subvec (clojure.string/split c #" ") 1 3)))))
-                         []
-                         (clojure.string/split rest #", "))]
-    (concat [parent] children)))
+  [rule regexp]
+  (re-seq regexp (clojure.string/replace rule #" bags| contain| no other|\.|," "")))
 
 (defn create-dependency-map
   {:test (fn []
-           (is= (create-dependency-map "light gray bags contain 5 dull maroon bags, 4 dull crimson bags, 5 mirrored cyan bags.\ndull maroon bags contain 1 mirrored chartreuse bag, 1 muted cyan bag.\nlight turquoise bags contain 2 bright yellow bags.")
+           (is= (create-dependency-map "light gray bags contain 5 dull maroon bags, 4 dull crimson bags, 5 mirrored cyan bags.\ndull maroon bags contain 1 mirrored chartreuse bag, 1 muted cyan bag.\nlight turquoise bags contain 2 bright yellow bags."
+                                       #"[a-z]+ [a-z]+")
                 {"light gray"      ["dull maroon" "dull crimson" "mirrored cyan"]
                  "dull maroon"     ["mirrored chartreuse" "muted cyan"]
-                 "light turquoise" ["bright yellow"]}))}
-  [input]
+                 "light turquoise" ["bright yellow"]})
+           (is= (create-dependency-map "light gray bags contain 5 dull maroon bags, 4 dull crimson bags, 5 mirrored cyan bags.\ndull maroon bags contain 1 mirrored chartreuse bag, 1 muted cyan bag.\nlight turquoise bags contain 2 bright yellow bags."
+                                       #"\d*[ ]?[a-z]+ [a-z]+")
+                {"light gray"      ["5 dull maroon" "4 dull crimson" "5 mirrored cyan"]
+                 "dull maroon"     ["1 mirrored chartreuse" "1 muted cyan"]
+                 "light turquoise" ["2 bright yellow"]}))}
+  [input parse-rule-regexp]
   (reduce (fn [m rule]
-            (let [parsed-rule (parse-rule rule)]
+            (let [parsed-rule (parse-rule rule parse-rule-regexp)]
               (assoc m (first parsed-rule) (rest parsed-rule))))
           {}
           (clojure.string/split-lines input)))
@@ -61,7 +67,7 @@
 
 (defn solve-a
   []
-  (let [dependency-map (create-dependency-map input)]
+  (let [dependency-map (create-dependency-map input #"[a-z]+ [a-z]+")]
     (reduce (fn [a k]
               (if (can-contain? dependency-map k "shiny gold")
                 (inc a)
@@ -73,35 +79,6 @@
   (solve-a)
   ; 172
   )
-
-(defn parse-rule-b
-  {:test (fn []
-           (is= (parse-rule-b "dotted maroon bags contain 4 plaid turquoise bags, 5 pale bronze bags.")
-                ["dotted maroon" "4 plaid turquoise" "5 pale bronze"])
-           (is= (parse-rule-b "plaid turquoise bags contain no other bags.")
-                ["plaid turquoise"]))}
-  [rule]
-  (let [[parent rest] (clojure.string/split rule #" bags contain ")
-        children (reduce (fn [a c]
-                           (if (= c "no other bags.")
-                             a
-                             (conj a (clojure.string/join " " (take 3 (clojure.string/split c #" "))))))
-                         []
-                         (clojure.string/split rest #", "))]
-    (concat [parent] children)))
-
-(defn create-dependency-map-b
-  {:test (fn []
-           (is= (create-dependency-map-b "light gray bags contain 5 dull maroon bags, 4 dull crimson bags, 5 mirrored cyan bags.\ndull maroon bags contain 1 mirrored chartreuse bag, 1 muted cyan bag.\nlight turquoise bags contain 2 bright yellow bags.")
-                {"light gray"      ["5 dull maroon" "4 dull crimson" "5 mirrored cyan"]
-                 "dull maroon"     ["1 mirrored chartreuse" "1 muted cyan"]
-                 "light turquoise" ["2 bright yellow"]}))}
-  [input]
-  (reduce (fn [m rule]
-            (let [parsed-rule (parse-rule-b rule)]
-              (assoc m (first parsed-rule) (rest parsed-rule))))
-          {}
-          (clojure.string/split-lines input)))
 
 (defn count-containing-bags
   {:test (fn []
@@ -126,7 +103,7 @@
 
 (defn solve-b
   []
-  (let [dependency-map (create-dependency-map-b input)]
+  (let [dependency-map (create-dependency-map input #"\d*[ ]?[a-z]+ [a-z]+")]
     (count-containing-bags dependency-map "shiny gold")))
 
 (comment
